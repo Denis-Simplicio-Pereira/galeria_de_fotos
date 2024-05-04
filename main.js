d60413f06a6f1b3a5e7edee2f26ca6753af5fdcb
Denis Simplicio Pereira <denis_simplicio@hotmail.com>
2024-05-04 10:20:15 -0300

    Galeria de fotos

--- main.js
+++ main.js
@@ -8,7 +8,19 @@ $(document).ready(function(){
     })
 
     $('form').on('submit', function(e){
-        console.log('submit');
         e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlat-imagem-link">
+                <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blank >
+                    Ver imagem em tamaho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(2000);
+        $('#endereco-imagem-nova').val('');
     })
 })
